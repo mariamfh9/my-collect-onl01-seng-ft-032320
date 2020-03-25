88a2f437b9a9f18d747567337713b3ff38cad938
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-25 18:26:56 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+arr = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
+end
+
+my_collect(arr) {|i| i.split(" ").first}
 
